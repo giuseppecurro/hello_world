512ff535f09ecfcfe119a14f062db6b57c00c409
giuseppecurro <giuseppe.curro1@gmail.com>
2019-11-07 09:29:26 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
    public static void main(String[] args) {
    
 
-      System.out.println("Sono tifoso dell'Inter");
+      System.out.println("Giuseppe Curro: Sono tifoso dell'Inter");
 
    }
 }
